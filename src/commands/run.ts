9764784ba67f36f93b4d503760596542a551f7d3
Juli Romero <juliiromero2@gmail.com>
2022-07-18 10:50:59 -0300

    Agrego logger en test y run ( logger.info y logger.error )

--- src/commands/run.ts
+++ src/commands/run.ts
@@ -2,6 +2,7 @@ import { Name, validate } from 'wollok-ts'
 import interpret from 'wollok-ts/dist/interpreter/interpreter'
 import natives from 'wollok-ts/dist/wre/wre.natives'
 import { buildEnvironmentForProject, failureDescription, problemDescription, successDescription, valueDescription } from '../utils'
+import  logger  from  'loglevel'
 
 const { time, timeEnd, log } = console
 
@@ -11,25 +12,26 @@ type Options = {
 }
 
 export default async function (programFQN: Name, { project, skipValidations }: Options): Promise<void> {
-  log(`Running ${valueDescription(programFQN)} on ${valueDescription(project)}`)
+  logger.info(`Running ${valueDescription(programFQN)} on ${valueDescription(project)}`)
 
   const environment = await buildEnvironmentForProject(project)
 
   if(!skipValidations) {
     const problems = validate(environment)
-    problems.forEach(problem => log(problemDescription(problem)))
-    if(!problems.length) log(successDescription('No problems found building the environment!'))
-    else if(problems.some(_ => _.level === 'error')) return log(failureDescription('Aborting run due to validation errors!'))
+    problems.forEach(problem => logger.info(problemDescription(problem)))
+    if(!problems.length) logger.info(successDescription('No problems found building the environment!'))
+    else if(problems.some(_ => _.level === 'error')) return logger.error(failureDescription('Aborting run due to validation errors!'))
   }
 
-  log(`Running ${valueDescription(programFQN)}...\n`)
+  logger.info(`Running ${valueDescription(programFQN)}...\n`)
 
   try {
-    time(successDescription('Run finalized successfully'))
+    const debug = logger.getLevel() <= logger.levels.DEBUG
+    if(debug) time(successDescription('Run finalized successfully'))
     interpret(environment, natives).run(programFQN)
-    timeEnd(successDescription('Run finalized successfully'))
+    if(debug) timeEnd(successDescription('Run finalized successfully'))
   } catch (error: any) {
-    log(failureDescription('Uh-oh... An error occurred during the run!', error))
+    logger.error(failureDescription('Uh-oh... An error occurred during the run!', error))
   }
 
   log()
