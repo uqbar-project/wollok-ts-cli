279c804ee8069de509bc31af4a6af811089e7aae
julimr <juliiromero2@gmail.com>
2022-08-25 12:43:40 -0300

    conexion socket y sketch

--- src/commands/run.ts
+++ src/commands/run.ts
@@ -1,13 +1,13 @@
 import { Evaluation, Name, validate } from 'wollok-ts'
-import interpret from 'wollok-ts/dist/interpreter/interpreter'
+import interpret, { Interpreter } from 'wollok-ts/dist/interpreter/interpreter'
 import natives from 'wollok-ts/dist/wre/wre.natives'
 import { buildEnvironmentForProject, failureDescription, problemDescription, successDescription, valueDescription } from '../utils'
 import  logger  from  'loglevel'
 import { Server } from 'socket.io'
 import express from 'express'
 import http from 'http'
-import path from 'path'
 import { app as client, BrowserWindow } from 'electron'
+import path from 'path'
 
 
 const { time, timeEnd, log } = console
@@ -16,12 +16,14 @@ type Options = {
   project: string
   skipValidations: boolean
 }
+let interp: Interpreter
+let projectPath : string
 
 export default async function (programFQN: Name, { project, skipValidations }: Options): Promise<void> {
   logger.info(`Running ${valueDescription(programFQN)} on ${valueDescription(project)}`)
-
+  
   const environment = await buildEnvironmentForProject(project)
-
+  projectPath = project
   if(!skipValidations) {
     const problems = validate(environment)
     problems.forEach(problem => logger.info(problemDescription(problem)))
@@ -30,24 +32,34 @@ export default async function (programFQN: Name, { project, skipValidations }: O
   }
 
   logger.info(`Running ${valueDescription(programFQN)}...\n`)
-  let interp
+
   try {
     const debug = logger.getLevel() <= logger.levels.DEBUG
     if(debug) time(successDescription('Run finalized successfully'))
-    // console.log(environment.members)
+
     interp = interpret(environment, natives)
     interp.run(programFQN)
-    // interp.evaluation.allInstances().forEach((obj) => console.log(obj.module.fullyQualifiedName()))
-    // console.log(interp.evaluation.allInstances())
+
     if(debug) timeEnd(successDescription('Run finalized successfully'))
   } catch (error: any) {
     logger.error(failureDescription('Uh-oh... An error occurred during the run!', error))
   }
 
-  let evaluation = interp?.evaluation
-  let titlee = evaluation?.send('title', evaluation?.object('wollok.game.game')).next().value
-  console.log(titlee)
+  const game = interp?.object('wollok.game.game')
+  const title = interp ? interp?.send('title', game!)?.innerString : 'Wollok Game'
+  const width = interp?.send('width', game!)?.innerNumber
+  const height = interp?.send('height', game!)?.innerNumber
+
+  const pathDirname = path.dirname(project)
+
+  const background = game.get('boardGround') ? game.get('boardGround')?.innerString : 'default'
+  const pathBackground = path.join(pathDirname,'/imagenes/', background! )
   
+  console.log(pathBackground)
+
+  let visuals = game.get('AllVisuals')
+  console.log(visuals?.innerCollection)
+
   log()
   const server = http.createServer(express())
     const io = new Server(server)
@@ -63,21 +75,24 @@ export default async function (programFQN: Name, { project, skipValidations }: O
           log(`Received pong from client with value: ${payload}`)
           count = payload
         })
+        socket.emit('fondo', pathBackground)
     })
     server.listen(3000)
 
     await client.whenReady()
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: width ? width*100 : 800,
+        height: height ? height*100 : 600,
         icon: __dirname + 'wollok.ico',
-        // title: titlee,
+        title: title,
         webPreferences: { 
             nodeIntegration: true,
+            contextIsolation: false
             }
     })
     
     win.removeMenu()
-    // win.webContents.openDevTools()
+    win.webContents.openDevTools()
+    console.log('dsp de devtools')
     win.loadFile('./public/indexGame.html')
 }
