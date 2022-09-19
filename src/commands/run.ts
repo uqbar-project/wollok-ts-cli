0be5ac96afcb87a31af6b7e26ee05791064bd037
Fernando Romero <fer.rom.mu@gmail.com>
2022-09-19 15:59:51 -0300

    Pair programming. Cambios en enviroment y native para poder lanzar un closure desde game.

--- src/commands/run.ts
+++ src/commands/run.ts
@@ -1,4 +1,4 @@
-import { Evaluation, InnerValue, Name, RuntimeObject, validate } from 'wollok-ts'
+import { Evaluation, InnerValue, link, parse, Name, RuntimeObject, validate } from 'wollok-ts'
 import interpret, { Interpreter } from 'wollok-ts/dist/interpreter/interpreter'
 import natives from 'wollok-ts/dist/wre/wre.natives'
 import { buildEnvironmentForProject, failureDescription, problemDescription, successDescription, valueDescription } from '../utils'
@@ -8,6 +8,7 @@ import express from 'express'
 import http from 'http'
 import { app as client, BrowserWindow } from 'electron'
 import path from 'path'
+import { appendFileSync } from 'fs'
 
 
 const { time, timeEnd, log } = console
@@ -21,13 +22,13 @@ type Options = {
   skipValidations: boolean
 }
 let interp: Interpreter
-let projectPath : string
 
 export default async function (programFQN: Name, { project, skipValidations }: Options): Promise<void> {
   logger.info(`Running ${valueDescription(programFQN)} on ${valueDescription(project)}`)
-  
-  const environment = await buildEnvironmentForProject(project)
-  projectPath = project
+
+  let environment = await buildEnvironmentForProject(project)
+  environment = link([parse.File('applier').tryParse('object applier{ method apply() native }')], environment)
+
   if(!skipValidations) {
     const problems = validate(environment)
     problems.forEach(problem => logger.info(problemDescription(problem)))
@@ -41,7 +42,14 @@ export default async function (programFQN: Name, { project, skipValidations }: O
     const debug = logger.getLevel() <= logger.levels.DEBUG
     if(debug) time(successDescription('Run finalized successfully'))
 
-    interp = interpret(environment, natives)
+    const nativesAndApply = {
+      ...natives,
+      applier: {
+        applier: { *apply() { console.log("llegamos? Sí. Llegamos.")} },
+      },
+    }
+
+    interp = interpret(environment, nativesAndApply)
     interp.run(programFQN)
 
     if(debug) timeEnd(successDescription('Run finalized successfully'))
@@ -57,90 +65,63 @@ export default async function (programFQN: Name, { project, skipValidations }: O
   const pathDirname = path.dirname(project)
 
   const background = game.get('boardGround') ? game.get('boardGround')?.innerString : 'default'
-  const pathBackground = path.join(pathDirname,'/',carpetaImgs,'/', background! )
-  const visualsImages = getImages(game,pathDirname)
-  const positions = getPositions(game,interp)
-  log()
+  const pathBackground = path.join(pathDirname, '/', carpetaImgs, '/', background! )
+  const visualsImages = getImages(game, pathDirname)
+  const positions = getPositions(game)
+
   const server = http.createServer(express())
   const io = new Server(server)
   const url = require('url');
 
-     io.on('connection', socket => {
-        log('Client connected!')
-        socket.on('disconnect', () => { log('Client disconnected!') })
+  io.on('connection', socket => {
+    log('Client connected!')
+    socket.on('disconnect', () => { log('Client disconnected!') })
 
-        socket.emit('getPathBackround', pathBackground)
-        socket.emit('VisualsImage', visualsImages)
-        socket.emit('VisualsPositions', positions)
+    let count = 0
 
-     })
-     server.listen(3000)
-
-    await client.whenReady()
-    const win = new BrowserWindow({
-        width: width ? width*50 : 800,
-        height: height ? height*50 : 600,
-        icon: __dirname + 'wollok.ico',
-        title: title,
-        webPreferences: { 
-            nodeIntegration: true,
-            contextIsolation: false
-            }
+    socket.on('pong', payload => {
+      log(`Received pong from client with value: ${payload}`)
+      count = payload
     })
+    socket.emit('getPathBackround', pathBackground)
+    socket.emit('VisualsImage', visualsImages)
+    socket.emit('VisualsPositions', positions)
+  })
+  server.listen(3000)
 
-    win.removeMenu()
-    win.webContents.openDevTools()
-    win.loadFile('./public/indexGame.html')
+  await client.whenReady()
+  const win = new BrowserWindow({
+    width: width ? width*50 : 800,
+    height: height ? height*50 : 600,
+    icon: __dirname + 'wollok.ico',
+    title: title,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  })
 
-    Singleton.Instance.start(io,interp,project)
+  win.removeMenu()
+  win.webContents.openDevTools()
+  win.loadFile('./public/indexGame.html')
+  interp.send('apply', interp.object('applier.applier'))
 }
-
-class Singleton {
-    private static instance: Singleton;
-
-    private constructor() {
-      if (Singleton.instance) {
-        return Singleton.instance;
-      }
-      Singleton.instance = this;
-    }
-    public static get Instance() {
-      return this.instance || (this.instance = new this());
-    }
-
-    public start(io : Server, interp : Interpreter, project: string){
-      setInterval(this.dibujar, 5000, io, interp, project)
-    }
-    public dibujar(io: Server, interp : Interpreter, project: string){
-      const game = interp.object('wollok.game.game')
-      const pathDirname = path.dirname(project)
-      const background = game.get('boardGround') ? game.get('boardGround')?.innerString : 'default'
-      const pathBackground = path.join(pathDirname,'/',carpetaImgs,'/', background! )
-      const visualsImages = getImages(game,pathDirname)
-      const positions = getPositions(game,interp)
-
-      io.emit('getPathBackround', pathBackground)
-      io.emit('VisualsImage', visualsImages)
-      io.emit('VisualsPositions', positions)
-     }
-
-}
-
 
 function getImages(game : RuntimeObject, pathProject : string){
-  let visualsImages: (string | number | boolean | Error | RuntimeObject[] | null | undefined)[] = []
+  const visualsImages: (string | number | boolean | Error | RuntimeObject[] | null | undefined)[] = []
   game.get('visuals')?.innerCollection?.forEach(v => {
-    let image = interp.send('image', v)?.innerString
+    const image = interp.send('image', v)?.innerString
     visualsImages.push(path.join(pathProject,'/',carpetaImgs,'/', image! ))
-   })
+  })
   return visualsImages
 }
-function getPositions(game: RuntimeObject, interp : Interpreter){
-  let positions: { x: InnerValue | undefined; y: InnerValue | undefined }[] = []
+
+function getPositions(game: RuntimeObject){
+  const positions: { x: InnerValue | undefined; y: InnerValue | undefined }[] = []
   game.get('visuals')?.innerCollection?.forEach(v => {
-    let x = interp.send('position', v)?.get('x')?.innerValue
-    let y = interp.send('position', v)?.get('y')?.innerValue
+    const x = interp.send('position', v)?.get('x')?.innerValue
+    const y = interp.send('position', v)?.get('y')?.innerValue
     positions.push({'x': x,'y':y})
-   })
+  })
   return positions
 }
