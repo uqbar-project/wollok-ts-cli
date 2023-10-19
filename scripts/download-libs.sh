4b74c3a4928e52322765eb2003c2ad8a9faa706d
nicolau <marcosnicolau2003@gmail.com>
2023-10-18 23:32:10 -0300

    fix: preinstall script (#100)
    fixes the preinstall script which was failing in windows

--- scripts/download-libs.sh
+++ scripts/download-libs.sh
@@ -2,11 +2,11 @@ LIB_FOLDER=./public/diagram/lib
 
 # socket.io
 if [ ! -f $LIB_FOLDER/socket.io.esm.min.js ]; then
-    wget -P $LIB_FOLDER "https://cdn.socket.io/4.4.1/socket.io.esm.min.js"
-    wget -P $LIB_FOLDER "https://cdn.socket.io/4.4.1/socket.io.esm.min.js.map"
+     curl -O --create-dirs --output-dir $LIB_FOLDER "https://cdn.socket.io/4.4.1/socket.io.esm.min.js"
+     curl -O --create-dirs --output-dir $LIB_FOLDER "https://cdn.socket.io/4.4.1/socket.io.esm.min.js.map"
 fi
 
 # cytoscape
 if [ ! -f $LIB_FOLDER/cytoscape.min.js ]; then
-    wget -P $LIB_FOLDER "https://cdnjs.cloudflare.com/ajax/libs/cytoscape/3.26.0/cytoscape.min.js"
+    curl -O --create-dirs --output-dir $LIB_FOLDER "https://cdnjs.cloudflare.com/ajax/libs/cytoscape/3.26.0/cytoscape.min.js"
 fi
