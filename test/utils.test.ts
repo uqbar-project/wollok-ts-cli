e7fd1198d1a78477b6b0fd1f26dd899aea9a67f9
Fernando Dodino <fernando.dodino@gmail.com>
2023-11-06 23:58:32 -0300

    Add test for utils & removing console infos

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,7 +1,10 @@
+import logger from 'loglevel'
+import sinon from 'sinon'
 import { join } from 'path'
-import { buildEnvironmentForProject, validateEnvironment } from '../src/utils'
+import { buildEnvironmentForProject, handleError, validateEnvironment } from '../src/utils'
 import chaiAsPromised from 'chai-as-promised'
 import chai from 'chai'
+import { spyCalledWithSubstring } from './assertions'
 
 describe('build & validating environment', () => {
 
@@ -21,6 +24,27 @@ describe('build & validating environment', () => {
   it('should not throw an exception if validation fails but you want to skip validation', async () => {
     const environment = await buildEnvironmentForProject(badProjectPath, ['fileWithValidationErrors.wlk'])
     chai.expect(() => { validateEnvironment(environment, true) }).to.not.throw()
+  })
+
+})
+
+describe('handle error', () => {
+
+  let loggerInfoSpy: sinon.SinonStub
+
+  beforeEach(() => {
+    loggerInfoSpy = sinon.stub(logger, 'error')
+  })
+
+  afterEach(() => {
+    sinon.restore()
+  })
+
+  it('shows error message', async () => {
+    await handleError(new Error('Parse validation failed'))
+
+    chai.expect(spyCalledWithSubstring(loggerInfoSpy, 'Uh-oh... Unexpected Error')).to.be.true
+    chai.expect(spyCalledWithSubstring(loggerInfoSpy, 'Parse validation failed')).to.be.true
   })
 
 })
