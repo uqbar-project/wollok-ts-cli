6e895c3a9a3f77a5a510f532d502f05d3e6357e1
Fernando Dodino <fernando.dodino@gmail.com>
2024-01-28 09:52:25 -0300

    Add time measurer test

---
+++ test/time-measurer.test.ts
@@ -0,0 +1,25 @@
+import { expect, should } from 'chai'
+import sinon from 'sinon'
+import { TimeMeasurer } from '../src/time-measurer'
+
+should()
+
+describe('Time Measurer', () => {
+
+  let clock: sinon.SinonFakeTimers
+
+  beforeEach(async () => {
+    clock = sinon.useFakeTimers()
+  })
+
+  afterEach(() => {
+    sinon.restore()
+  })
+
+  it('time elapsed is calculated based on hrtime', () => {
+    const timeMeasurer = new TimeMeasurer()
+    clock.tick(2000)
+    expect(timeMeasurer.elapsedTime()).to.equal(2000)
+  })
+
+})
