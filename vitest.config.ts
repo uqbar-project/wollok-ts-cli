4538ae4f014bd7a779e7e7efa02951151dc3d9b6
fdodino <fernando.dodino@gmail.com>
2025-09-14 23:22:11 -0300

    Fix coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,10 +4,11 @@ export default defineConfig({
   test: {
     globals: false,
     include: ['test/**/*.test.ts'],
-    exclude: ['node_modules', 'dist'],
+    exclude: ['node_modules', 'dist', 'scripts', 'public'],
     environment: 'node',
     coverage: {
       provider: 'v8',
+      exclude: ['node_modules', 'dist', 'scripts', 'public', '*.config.{ts,js}'],
       reporter: ['text', 'lcov']
     },
   },
